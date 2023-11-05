00d1cce6d18d42860276af9d70828f770944a6f0
KayoticCarnige <32397453+kckarnige@users.noreply.github.com>
2023-11-05 12:55:05 -0500

    Working on fixes and auto updater

--- index.js
+++ index.js
@@ -1,24 +1,27 @@
 const { app, Menu, Tray, dialog } = require("electron");
 const path = require("path");
 
-let tray;
+var
+tray = null,
+trayicon = path.join(__dirname, "./icon.ico"),
+startUpDialogClose = false;
 
-const options0 = {
+const
+options0 = {
   type: "question",
   buttons: ["OK"],
   title: "Oculus Dummy",
   message:
     "You can close this program by right-clicking the icon in your system tray.\nTo avoid issues, please only do this when you're not in VR.",
-};
-
-const options1 = {
+},
+options1 = {
   type: "question",
   buttons: ["OK", "Cancel"],
   title: "Oculus Dummy",
   message:
     "Are you sure you want to close Oculus Dummy?\nOnly close it when you're not in VR, doing so usually usually just restarts Oculus Dummy, but to be safe and avoid issues, it's recommended you only do it when not in VR.",
 };
-let startUpDialogClose = false;
+
 app.disableHardwareAcceleration();
 Menu.setApplicationMenu(null);
 app.whenReady().then(() => {
@@ -28,7 +31,7 @@ app.whenReady().then(() => {
       startUpDialogClose = true;
     }
   });
-  tray = new Tray(path.join(__dirname, "./icon.ico"));
+  tray = new Tray(trayicon);
   tray.setToolTip("Oculus Dummy");
   tray.setIgnoreDoubleClickEvents(true);
   tray.on("right-click", () => {
